698ac55269770953d3120d015ab8b82f43edfd6b
ferdi <ferdinandmond@gmail.com>
2022-11-27 11:45:27 +0100

    broken version

--- energyscheduling.py
+++ energyscheduling.py
@@ -1,5 +1,6 @@
 from parse_instance import read_instance
 import numpy as np
+import random
 
 def build_minimal(inst,filename):
     N=inst.get('N')
@@ -12,42 +13,110 @@ def build_minimal(inst,filename):
     # check if those with priority=1 are in the keys of predecessor, if so take their values and set priority=2
     # loop through until there are no more keys found
     # have priority list, higher value higher priority
+    #print(inst)
+    free_to_schedule=[]
+    free_to_schedule_work=[]
+    for i in range(1,N+1):
+        free_to_schedule.append(i)
+        #free_to_schedule_work.append(inst.get('work')[i])
     for i in range(N):
         if i in preds:
             if priority[i-1]==0:
                 priority[i-1] = 1
+                free_to_schedule.remove(i)
+                #free_to_schedule_work.remove(i)
             for j in preds.get(i):
                 priority[j-1] = 2
+                if j in free_to_schedule:
+                    free_to_schedule.remove(j)
+                    #free_to_schedule_work.pop(i)
     flag = True
     current_max=2
+    for i in free_to_schedule:
+        #print(i)
+        free_to_schedule_work.append(inst.get('work')[i-1])
+    print(len(free_to_schedule))
+    #print(free_to_schedule_work)
+
+    
     while(flag):
         flag=False
         for i in range(1,N+1):
-    	    if(priority[i-1]==current_max):
-    	        if i in preds:
-    	            for j in preds.get(i):
-    	                priority[j-1]=current_max+1
-    	                flag=True
+            if(priority[i-1]==current_max):
+                if i in preds:
+                    for j in preds.get(i):
+                        priority[j-1]=current_max+1
+                        flag=True
         current_max=current_max+1
-
+    #print(priority)
     scheduling = {}
     cpu_busy = {}
     current_process=1
-    for i in range(current_max,-1,-1):
+    for i in range(current_max-1,0,-1):
+        free=[*range(1,M+1)]
+        scheduled=[]
+        #print("Priority: " + str(i))
         for j in range(N):
             if(priority[j] == i):
                 start_time = 0
+                #print(current_process)
                 if j+1 in preds:
                     start_time=0
                     for k in preds.get(j+1):
                         if scheduling.get(k)[-1] > start_time:
                             start_time = scheduling.get(k)[-1]
+                            if scheduling.get(k)[1] in free:
+                                #print("process its trying to schedule to: " + str(scheduling.get(k)[1]))
+                                #print(free)
+                                current_process=scheduling.get(k)[1]
                 if current_process in cpu_busy:
                     start_time = max(cpu_busy.get(current_process),start_time)
                 runtime = inst.get('work')[j]/inst.get('freq_list')[inst.get('S')-1]
                 scheduling[j+1] = [j+1, current_process, inst.get('S'), start_time, start_time+runtime]
                 cpu_busy[current_process] = start_time+runtime
-                current_process = (current_process)%M+1
+                #print("process trying to be removed from free: " + str(current_process))
+                free.remove(current_process)
+                #print(free)
+                scheduled.append(current_process)
+                #current_process = (current_process)%M+1
+                if not free:
+                    free=[*range(1,M+1)]
+                current_process = random.choice(free)
+        #print(scheduling)
+        #print("\n\n")
+        #second path, prio 0
+        #print(free)
+        #print(cpu_busy)
+        for i in free:
+            if not free_to_schedule:
+                print('break')
+                break
+            if len(free)==1:
+                print('len=1')
+                current = free_to_schedule[free_to_schedule_work.index(min(free_to_schedule_work))]
+            else:
+                current = random.choice(free_to_schedule)
+            current_process = random.choice(free)
+            if current_process in cpu_busy:
+                start_time=cpu_busy.get(current_process)
+            else:
+                start_time=0
+            #print(start_time)
+            runtime = inst.get('work')[current]/inst.get('freq_list')[inst.get('S')-1]
+            #print('scheduling ' + str(current)+' from '+str(start_time)+' to ' + str(start_time+runtime))
+            scheduling[current] = [current, current_process, inst.get('S'), start_time, start_time+runtime]
+            cpu_busy[current_process] = start_time+runtime
+            free.remove(current_process)
+            scheduled.append(current_process)
+            free_to_schedule_work.pop(free_to_schedule.index(current))
+            free_to_schedule.remove(current)
+            print(free)
+            
+        if not free:
+            free=[*range(1,M+1)]
+
+                
+
     makespan = 0
     for i in range(1,M+1):
         if(i in cpu_busy):
@@ -61,6 +130,9 @@ def build_minimal(inst,filename):
         idletime+=scheduling.get(i)[4]-scheduling.get(i)[3]
     idletime = makespan*M-idletime
     energy+=idletime*inst.get('power_list')[0]
+    #print(preds)
+    #print(priority)
+    print(makespan)
     with open(filename+'_output.dat', 'w') as outfile:
         outfile.write(str(N)+'\n')
         outfile.write(str(makespan)+'\n')
@@ -70,9 +142,9 @@ def build_minimal(inst,filename):
     
 
 def main():
-    filename = "instances/student_instance_4"
+    filename = "instances/student_instance_3"
     inst = read_instance(filename+'.dat')
-    print(inst)
+    #print(inst)
     build_minimal(inst, filename)
 
 
